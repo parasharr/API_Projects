80d111e40edb2fce084da1c26bc3d70d44fc0891
PranjeetGoswami <prgoswami2020@gmail.com>
2024-09-30 19:58:19 +0530

    add to repo

---
+++ Dictionary/script.js
@@ -0,0 +1,46 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+
+const btn = document.getElementById("search_btn");
+
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                <h3>${inpWord}</h3>
+                <button onclick="playSound()">
+                    <i class="fas fa-volume-up"></i>
+                </button>
+            </div>
+            <div class="details">
+                <p>${data[0].meanings[0].partOfSpeech}</p>
+                <p>/${data[0].phonetic}/</p>
+            </div>
+            <p class="word-meaning">
+                ${data[0].meanings[0].definitions[0].definition}
+            </p>
+            <p class="word-example">
+                ${data[0].meanings[0].definitions[0].example || ""}
+            </p>`;
+
+            // Check if the audio URL is available and valid
+            const audioUrl = data[0].phonetics.find(phonetic => phonetic.audio);
+            if (audioUrl) {
+                sound.setAttribute("src", audioUrl.audio);
+            } else {
+                console.error("Audio URL not found");
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        });
+});
+
+function playSound() {
+    sound.play();
+}
